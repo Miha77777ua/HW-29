3a60ba525800dc689025b69451a7a2f589863cdf
Miha <lv11miha@gmail.com>
2026-02-07 01:35:59 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,4 @@
 import { createStore } from "redux";
 import { rootReducer } from "./reducers";
-import { devToolsEnhancer } from "@redux-devtools/extension";
 
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+export const store = createStore(rootReducer);
